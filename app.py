0a0c3e57913ef688c539b030652749a2194e0212
Erick Andrews <andrews_erick@yahoo.com>
2024-07-04 00:15:17 +0000

    built date app and docker container, ran on server

---
+++ app.py
@@ -0,0 +1,13 @@
+﻿from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/time', methods=['GET'])
+def get_time():
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return jsonify({'current_time': current_time})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
